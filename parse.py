627249b32e71e914931ddf988e4988714e207f23
Nick Imkamp <imkampn@gamil.com>
2016-12-22 15:26:48 -0800

    Used hasmap/dictionary to optimize function

--- parse.py
+++ parse.py
@@ -1,62 +1,50 @@
-def letterCount( file, letter ):
-    count = 0
+from collections import defaultdict
 
-    inFile = open( file, 'r' )
-    contents = inFile.read()
-
-    for character in contents:
-        if character == letter:
-            count += 1
-    inFile.close()
-    print( "Number of occurrences: " + str(count) )
+# def letterCount( file, letter ):
+#     count = 0
+#
+#     inFile = open( file, 'r' )
+#     contents = inFile.read()
+#
+#     for character in contents:
+#         if character == letter:
+#             count += 1
+#     inFile.close()
+#     print( "Number of occurrences: " + str(count) )
 
 def largestWord( file ):
     inFile = open( file )
     contents = inFile.read()
-
-    maxLength = 0
-    biggestWord = ""
-    count = 0
+    dict = defaultdict()
 
     for word in contents.split():
-        lengthOfWord = len( word )
-        bigWord = word
-
-        if lengthOfWord > maxLength:
-            maxLength = lengthOfWord
-            biggestWord = bigWord
-
-            if word == biggestWord:
-                count += 1
-
+        dict[word] = len(word)
+    for word in sorted(dict, key=dict.get, reverse=True):
+        print word, dict[word]
     inFile.close()
+    
+# def locator(file, word):
+#     line_num = 0
+#     inFile = open(file)
+#     for line in inFile.readlines():
+#         line_num += 1
+#         if line.find(word) >= 0:
+#             print("line number: " + str(line_num))
+#     inFile.close()
 
-    print( "Length of the largest word: " + str(maxLength) )
-    print( "Largest word: " + biggestWord )
-    print("Occurrences: " + str(count) )
-
-def locator(file, word):
-    line_num = 0
-    inFile = open(file)
-    for line in inFile.readlines():
-        line_num += 1
-        if line.find(word) >= 0:
-            print("line number: " + str(line_num))
-    inFile.close()
-
-def textFileReverse(file):
-    inFile = open(file)
-    contents = inFile.readlines()
-    for line in reversed(contents):
-        print(line.rstrip())
-    inFile.close()
+# def textFileReverse(file):
+#     inFile = open(file)
+#     contents = inFile.readlines()
+#     for line in reversed(contents):
+#         print(line.rstrip())
+#     inFile.close()
 
 if __name__ == "__main__":
 
-    textFileReverse("textFile.txt")
-    letterCount("textFile.txt", "a")
+    # textFileReverse("textFile.txt")
+    # letterCount("textFile.txt", "a")
     largestWord("textFile.txt")
-    locator('textFile.txt', "Writing")
+    # locator('textFile.txt', "Writing")
 
 
 
